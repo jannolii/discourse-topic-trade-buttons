bc702deb4787676fa28e4021c6e6c1092ee46cb6
Janno Liivak <janno@edev.ee>
2017-03-18 18:36:12 +0200

    Fix locales. Add plugin enableing line.

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,5 @@
 # about: Adds one or all buttons (Sold, Purchased, Exchanged) to designated categories
 # version: 0.0.1
 # authors: Janno Liivak
+
+enabled_site_setting :topic_trade_buttons_enabled
