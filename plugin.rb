7b3cc23c8130cd27dab6b9c7f9c86f5c2628ffcc
Janno Liivak <janno@edev.ee>
2017-09-29 18:30:40 +0300

    Replace deprecated before_filter

--- plugin.rb
+++ plugin.rb
@@ -92,7 +92,7 @@ after_initialize do
   class DiscourseTopicTradeButtons::TradeController < ::ApplicationController
     requires_plugin PLUGIN_NAME
 
-    before_filter :ensure_logged_in
+    before_action :ensure_logged_in
 
     def sold
       topic_id   = params.require(:topic_id)
